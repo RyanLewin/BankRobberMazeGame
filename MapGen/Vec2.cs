80d1f734f8a086c1547e511e415fdc12ee1a09fc
RyanLewin <ryanjlewin@gmail.com>
2017-01-09 23:24:00 +0000

    Revert "Clean ut"
    This reverts commit 34ec702d5ad81c97a1c250dac7e371828562d207.

---
+++ MapGen/Vec2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct Vec2
+{
+    public int x, y;
+    public Vec2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Vec2 operator +(Vec2 a, Vec2 b)
+    {
+        a.x += b.x;
+        a.y += b.y;
+        return a;
+    }
+}
